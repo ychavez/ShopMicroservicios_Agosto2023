be8ff313a31e66304ee3959ca90ec664a1f3ab81
Yael Chavez <you400@gmail.com>
2023-08-12 11:55:29 -0600

    docker

---
+++ Catalog.Api/Controllers/CatalogController.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts() 
+        {
+            return Ok();
+        }
+
+    }
+}
